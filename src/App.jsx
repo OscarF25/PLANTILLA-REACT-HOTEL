f2e9d216777529e45879918aac9eebdb0b6638ed
Programacion79 <Programacion1979>
2025-05-07 07:08:37 -0500

    Se crea la pagina RoomDetailsPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import RoomsPage from "./pages/RoomsPage";
+import RoomDetailsPage from "./pages/RoomDetailsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/About" element={<AboutPage />} />
         <Route path="/Rooms" element={<RoomsPage />} />
+        <Route path="/Room/:id" element={<RoomDetailsPage />} />
         <Route path="/Contact" element={<ContactPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
---
+++ src/pages/RoomDetailsPage.jsx
@@ -0,0 +1,10 @@
+
+
+export default function RoomDetailsPage() {
+  return (
+    <div>
+        <h1>DETALLE HABITACION</h1>
+        <h2>Habitacion #</h2>
+    </div>
+  )
+}
